04c69e505dffe24dbf12828acf8b30ac78eac634
Aniko Kopis-Horvath <ha891209@gmail.com>
2021-06-21 23:04:53 +0200

    solution added

--- nodejs-alapjai-yargs/app.js
+++ nodejs-alapjai-yargs/app.js
@@ -1,54 +1,61 @@
-const yargs = require('yargs')
-const { id, producer, title } = require('./option')
-const {
-    getAllMovies,
-    findMovieById,
-    createMovie,
-    editMovie,
-    removeMovie
-} = require('./movies.service')
+const yargs = require('yargs');
+const { id, producer, title } = require('./options');
+const MovieApi = require('./movie.api');
+const MovieService = require('./movie.service');
 
-yargs
-    .version('1.0.0')
-    .usage('Usage: <command> [options]')
-    .command({
-        command: 'get',
-        describe: 'Get all movies',
-        handler: () => console.log(getAllMovies())
-    })
-    .command({
-        command: 'find',
-        describe: 'Find a movie by id',
-        builder: { id },
-        handler: ({ id }) =>
-            console.log(findMovieById(id))
-    })
-    .command({
-        command: 'create',
-        describe: 'Create a new movie',
-        builder: { producer, title },
-        handler: ({ producer, title }) => {
-            console.log(createMovie(producer, title))
-        }
-    })
-    .command({
-        command: 'edit',
-        describe: 'Edit a movie',
-        builder: { id, producer, title },
-        handler: ({ id, producer, title }) => {
-            console.log(editMovie(id, producer, title))
-        }
-    })
-    .command({
-        command: 'remove',
-        describe: 'Remove a movie by ID',
-        builder: { id },
-        handler: ({ id }) => {
-            removeMovie(id)
-            console.log('deleted')
-        }
-    })
-    .locale('en')
-    .strict()
-    .help()
-    .parse() // process.argv
+const moviesApi = MovieApi('./database/movies.json', 'movies');
+(async () => {
+
+    const {
+        getAllMovies,
+        findMovieById,
+        createMovie,
+        editMovie,
+        removeMovie
+    } = await MovieService(moviesApi);
+
+    yargs
+        .version('1.0.0')
+        .usage('Usage: <command> [options]')
+        .command({
+            command: 'get',
+            describe: 'Get all movies',
+            handler: () => console.log(getAllMovies())
+        })
+        .command({
+            command: 'find',
+            describe: 'Find a movie by id',
+            builder: { id },
+            handler: (args) =>
+                console.log(findMovieById(args.id))
+        })
+        .command({
+            command: 'create',
+            describe: 'Create a new movie',
+            builder: { producer, title },
+            handler: async (args) => {
+                console.log(await createMovie(args))
+            }
+        })
+        .command({
+            command: 'edit',
+            describe: 'Edit a movie',
+            builder: { id, producer, title },
+            handler: async (args) => {
+                console.log(await editMovie(args))
+            }
+        })
+        .command({
+            command: 'remove',
+            describe: 'Remove a movie by ID',
+            builder: { id },
+            handler: async (args) => {
+                await removeMovie(args.id)
+                console.log('deleted')
+            }
+        })
+        .locale('en')
+        .strict()
+        .help()
+        .parse();
+})();
